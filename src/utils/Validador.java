1e6d8bf663c24e10265c8b4b8574755aaa061b9f
shirlyk89 <226595573+shirlyk89@users.noreply.github.com>
2026-06-02 10:51:42 -0500

    chore: Validaciones(stock, precio, formato correos, numero ID o CEl)

--- src/utils/Validador.java
+++ src/utils/Validador.java
@@ -1,4 +1,32 @@
 package utils;
 
+import java.util.regex.Pattern;
+
 public class Validador {
+
+    // Validar precio y stock positivos
+
+    public static boolean validarStock(int stock){
+        return stock > 0;
+    }
+
+    public static boolean validarPrecio(double precio){
+        return precio > 0;
+    }
+
+    // validar formato del correo
+
+    private static final String EMAIL_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+
+    public static boolean validarCorreo(String correo){
+        if (correo==null) return false;
+        return Pattern.matches(EMAIL_REGEX, correo);
+    }
+
+    // validar numero de identificacion unico
+
+    public static boolean validarNumero(String texto){
+        if (texto == null)return false;
+        return texto.matches("\\d+");
+    }
 }
