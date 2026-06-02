e0218e9f279c2fdc4b1b767a924e90d9435840a4
Luis Esteban Ramirez Campo <226594654+L0campo@users.noreply.github.com>
2026-06-02 11:33:39 -0500

    class cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,41 @@
 package model;
 
 public class Cliente {
+    private int id;
+    private String nombre;
+    private String identificacion;
+    private String correo;
+    private String telefono;
+
+    public Cliente() {}
+
+    // Constructor 
+    public Cliente(int id, String nombre, String identificacion, String correo, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getIdentificacion() { return identificacion; }
+    public void setIdentificacion(String identificacion) { this.identificacion = identificacion; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    @Override
+    public String toString() {
+        return "Cliente [" + identificacion + "] " + nombre + " - Email: " + correo;
+    }
 }
