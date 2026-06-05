ca731ef05ed79d45f608b8640dc9c3bf24ba4eee
Luis Esteban Ramirez Campo <226594654+L0campo@users.noreply.github.com>
2026-06-05 12:15:56 -0500

    Implement discount strategy in DescuentoFeriado class

--- src/patron/DescuentoFeriado.java
+++ src/patron/DescuentoFeriado.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package patron;
 
-/**
- *
- * @author SHIRLY LEAL
- */
-public class DescuentoFeriado {
-    
+public class DescuentoFeriado implements EstrategiaDescuento {
+    @Override
+    public double aplicarDescuento(double subtotal) {
+        return subtotal * 0.90; // Descuenta el 10%
+    }
 }
