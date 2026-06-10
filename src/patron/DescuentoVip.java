6138216bad1e727b962bf1d05b7ad9ad3a2edf7e
Luis Esteban Ramirez Campo <226594654+L0campo@users.noreply.github.com>
2026-06-09 19:49:27 -0500

    Fix class name casing for DescuentoVip

--- src/patron/DescuentoVip.java
+++ src/patron/DescuentoVip.java
@@ -1,6 +1,6 @@
 package patron;
 
-public class DescuentoVIP implements EstrategiaDescuento {
+public class DescuentoVip implements EstrategiaDescuento {
     @Override
     public double aplicarDescuento(double subtotal) {
         return subtotal * 0.85; // Descuenta el 15%
